2539302aeec228f1cc4034f9a1b0e0abab5b27dc
iKaushalkumarPatel <47134112+iKaushalkumarPatel@users.noreply.github.com>
2019-01-28 23:43:37 -0600

    Program1
    Program1_HelloWorld - Java program to display Hello World.

---
+++ Program1_HelloWorld.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Program1_HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("Hello World");
+     }
+}
