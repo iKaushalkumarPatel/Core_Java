784b5606a8b6f78bbd7ddfbb6de8a3c83ee6fc33
iKaushalkumarPatel <47134112+iKaushalkumarPatel@users.noreply.github.com>
2019-01-28 23:49:03 -0600

    Program2
    Program2_DisplayYourName - java program to display your name.

---
+++ Program2_DisplayYourName.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Program1_HelloWorld{
+
+     public static void main(String []args){
+        String name="Kaushalkumar Patel"; 
+        System.out.println("Hello\t" +name);
+     }
+}
